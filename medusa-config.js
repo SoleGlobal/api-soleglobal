6d217af46ae8fcc635cae5f1f09287bd5d4e3871
Oskar Kaptacz <oskarkaptacz@gmail.com>
2023-08-14 21:05:29 +0200

    minio

--- medusa-config.js
+++ medusa-config.js
@@ -55,11 +55,17 @@ const plugins = [
     	options: {
         endpoint: process.env.MINIO_ENDPOINT,
         bucket: process.env.MINIO_BUCKET,
+        access_key_id: process.env.MINIO_ACCESS_KEY,
+        secret_access_key: process.env.MINIO_SECRET_KEY,
+         /*
+        endpoint: process.env.MINIO_ENDPOINT,
+        bucket: process.env.MINIO_BUCKET,
         key_id: process.env.MINIO_ACCESS_KEY,
         access_key: process.env.MINIO_SECRET_KEY,
         private_bucket: process.env.MINIO_BUCKET,
         private_access_key_id: process.env.MINIO_ACCESS_KEY,
         private_secret_access_key: process.env.MINIO_SECRET_KEY,
+        */
        },
      },
   /*   {	
